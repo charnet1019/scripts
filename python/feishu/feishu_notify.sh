62c1828109c0d4bf30772e29b1bb5fd26176e178
charnet1019 <charnet1019@163.com>
2021-05-14 10:36:10 +0800

    Update feishu_notify.sh

--- python/feishu/feishu_notify.sh
+++ python/feishu/feishu_notify.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-PUBLISH_TIME=$(date "+%Y-%m-%d %T")
-PROJECT_NAME="iot"
-#PROJECT_SUBMODULE="bar-patrol"
-PROJECT_SUBMODULE="${1}"
+# 调用飞书接口无签名验证
 
-#curl -X POST \
-#  https://open.feishu.cn/open-apis/bot/v2/hook/49c2a27b-eeea-425f-xxxxx \
-#  -H 'Content-Type: application/json' \
-#  -d '{
-#    "msg_type": "text",
-#    "content": {
-#        "text": "'"物联网项目${PROJECT_SUBMODULE}模块已上线,请稍后验证."'"
-#    }
-#}'
+PUBLISH_TIME=$(date "+%Y-%m-%d %T")
+PROJECT_NAME="myproject"
+PROJECT_SUBMODULE="${1}"
 
 
 curl -X POST \
-  https://open.feishu.cn/open-apis/bot/v2/hook/49c2a27b-eeea-xxxxxxxxxxxxx \
+  https://open.feishu.cn/open-apis/bot/v2/hook/49c2a27b-eeea-425f-a818-1cf463f4ebb2 \
   -H 'Content-Type: application/json' \
   -d "{
     \"msg_type\": \"text\",
@@ -25,5 +16,3 @@ curl -X POST \
         \"text\": \"${PROJECT_SUBMODULE} 已于 ${PUBLISH_TIME} 发布，请稍后验证改动内容。\"
     }
 }"
-
-
